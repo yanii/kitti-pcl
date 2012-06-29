8a8c91213a1d4fb599b313cf3acfddaba72e8a75
Yani Ioannou <yani@cs.utoronto.ca>
2012-06-29 16:04:12 -0400

    Add frame id

--- src/kittitrackletextract.cpp
+++ src/kittitrackletextract.cpp
@@ -29,6 +29,9 @@ int main(int argc, char **argv){
 	///The object type to extract
 	string objtype;
 
+	///The kitti frame id
+	int frameid;
+
 	// Declare the supported options.
 	po::options_description desc("Program options");
 	desc.add_options()
@@ -37,7 +40,8 @@ int main(int argc, char **argv){
 		//Options
 		("infile", po::value<string>(&infile)->required(), "the file to read a point cloud from")
 		("tracklets", po::value<string>(&trackletfile)->required(), "the file to read kitti tracklet annotations from")
-		("outfile", po::value<string>(&outfile)->required(), "the file to write the DoN point cloud & normals to")
+		("frameid", po::value<int>(&frameid)->required(), "the kitti frame id")
+		("outfile", po::value<string>(&outfile)->required(), "the file prefix to write the tracklet segmentations to")
 		("type", po::value<string>(&objtype), "the name of the object type to extract (default: all)")
 		;
 	// Parse the command line
@@ -76,13 +80,13 @@ int main(int argc, char **argv){
 
 	//For each tracklet, extract the points
 	for(int i = 0; i < tracklets->numberOfTracklets(); i++){
-		if(!tracklets->isActive(i,0)){
+		if(!tracklets->isActive(i,frameid)){
 			continue;
 		}
 		Tracklets::tTracklet* tracklet = tracklets->getTracklet(i);
 		if(objtype.empty() || tracklet->objectType == objtype){
 			Tracklets::tPose *pose;
-			if(tracklets->getPose(i, 0, pose)){
+			if(tracklets->getPose(i, frameid, pose)){
 				outcloud.reset(new pcl::PointCloud<PointXYZI>);
 				clipper.setTranslation(Eigen::Vector3f(pose->tx, pose->ty, pose->tz));
 				clipper.setRotation(Eigen::Vector3f(pose->rx, pose->ry, pose->rz));
