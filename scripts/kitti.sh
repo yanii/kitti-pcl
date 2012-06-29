edce43e26493080cb31bfa708e6c90b17e03e448
Yani Ioannou <yani@cs.utoronto.ca>
2012-06-29 16:20:02 -0400

    Fix script

--- scripts/kitti.sh
+++ scripts/kitti.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-for file in *.bin; do kitti2pcd --infile $file --outfile `basename $file .bin`.pcd; mkdir `basename $file .bin`; kittitrackletextract --infile `basename $file .bin`.pcd --outfile `basename $file .bin`/`basename $file .bin`_ --tracklets ../../tracklet_labels.xml --frameid `basename $file .bin`; done
+for file in *.bin
+do 
+	kitti2pcd --infile $file --outfile `basename $file .bin`.pcd
+	mkdir `basename $file .bin`; kittitrackletextract --infile `basename $file .bin`.pcd --outfile `basename $file .bin`/`basename $file .bin`_ --tracklets ../../tracklet_labels.xml --frameid `basename $file .bin`
+done
